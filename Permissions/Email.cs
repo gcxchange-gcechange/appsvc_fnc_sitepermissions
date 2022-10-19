e874deb530026462c034d057da86068a0efcbc59
doug0102 <shea.wow@hotmail.com>
2022-10-19 14:49:09 -0400

    fix email indentation

--- Permissions/Email.cs
+++ Permissions/Email.cs
@@ -80,40 +80,39 @@ namespace SitePermissions
                     Body = new ItemBody
                     {
                         ContentType = BodyType.Text,
-                        Content = @$"
-                        (La version française suit)
+                        Content = @$"(La version française suit)
 
-                        Dear { Username }, 
+Dear { Username }, 
 
-                        Thank you for your interest and participation as a Community Owner on the GCXchange platform. 
+Thank you for your interest and participation as a Community Owner on the GCXchange platform. 
 
-                        Communities continue to be our most popular feature of the platform, and we look forward to seeing new and interesting communities appear as the platform grows.
+Communities continue to be our most popular feature of the platform, and we look forward to seeing new and interesting communities appear as the platform grows.
 
-                        One of the key features of GCXchange is the open by default model which allows for seamless cross-departmental collaboration. We encourage all site owners to maintain the open by default practice as outlined in our Terms of Use. 
+One of the key features of GCXchange is the open by default model which allows for seamless cross-departmental collaboration. We encourage all site owners to maintain the open by default practice as outlined in our Terms of Use. 
 
-                        The GCXchange Team received a notification that the permissions were modified to the Community space in which you are Owner. This action goes against the open by default model of the platform. Please note that the default settings will now be reapplied, and we would like to give you a friendly reminder to please no longer change these permissions. 
+The GCXchange Team received a notification that the permissions were modified to the Community space in which you are Owner. This action goes against the open by default model of the platform. Please note that the default settings will now be reapplied, and we would like to give you a friendly reminder to please no longer change these permissions. 
 
-                        Please let us know if you have any questions or concerns, and once again thank you for being a valued member of GCXchange.  
-                         
-                        Regards, 
-                        The GCX Team 
+Please let us know if you have any questions or concerns, and once again thank you for being a valued member of GCXchange.  
+ 
+Regards, 
+The GCX Team 
 
-                        --------------------------------------
+--------------------------------------
 
-                        Bonjour { Username }, 
+Bonjour { Username }, 
 
-                        Nous vous remercions de l’intérêt que vous manifestez à l’égard de la plateforme GCÉchange, de même que de votre participation en tant que responsable de l’une de ses collectivités. 
+Nous vous remercions de l’intérêt que vous manifestez à l’égard de la plateforme GCÉchange, de même que de votre participation en tant que responsable de l’une de ses collectivités. 
 
-                        Les collectivités demeurent la fonction la plus prisée de la plateforme, et nous sommes impatients de voir de nouvelles collectivités intéressantes se créer, au fur et à mesure que la plateforme évolue. 
+Les collectivités demeurent la fonction la plus prisée de la plateforme, et nous sommes impatients de voir de nouvelles collectivités intéressantes se créer, au fur et à mesure que la plateforme évolue. 
 
-                        L’une des principales caractéristiques de GCÉchange est son modèle ouvert par défaut, qui permet une collaboration permanente entre les ministères. Nous encourageons tous les responsables à maintenir la pratique d’ouverture par défaut, comme indiqué dans nos conditions d’utilisation. 
+L’une des principales caractéristiques de GCÉchange est son modèle ouvert par défaut, qui permet une collaboration permanente entre les ministères. Nous encourageons tous les responsables à maintenir la pratique d’ouverture par défaut, comme indiqué dans nos conditions d’utilisation. 
 
-                        L’équipe de GCÉchange a reçu un avis concernant la modification des autorisations relatives à la collectivité dont vous êtes responsable. Cette modification va à l’encontre du modèle ouvert par défaut de la plateforme. Veuillez prendre note que les paramètres par défaut seront rétablis, et nous vous prions de ne plus modifier les autorisations. 
+L’équipe de GCÉchange a reçu un avis concernant la modification des autorisations relatives à la collectivité dont vous êtes responsable. Cette modification va à l’encontre du modèle ouvert par défaut de la plateforme. Veuillez prendre note que les paramètres par défaut seront rétablis, et nous vous prions de ne plus modifier les autorisations. 
 
-                        N’hésitez pas à nous faire part de vos questions et de vos préoccupations. Vous êtes un membre important de GCÉchange et nous souhaitons réitérer notre gratitude.  
-                         
-                        Nous vous prions d’agréer l’expression de nos sentiments les meilleurs. 
-                        Équipe de GCÉchange"
+N’hésitez pas à nous faire part de vos questions et de vos préoccupations. Vous êtes un membre important de GCÉchange et nous souhaitons réitérer notre gratitude.  
+ 
+Nous vous prions d’agréer l’expression de nos sentiments les meilleurs. 
+Équipe de GCÉchange"
                     },
                     ToRecipients = new List<Recipient>()
                     {
