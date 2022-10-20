ac24c0fa58e42d0393de6bf5386ffd0cab1cfe2f
doug0102 <shea.wow@hotmail.com>
2022-10-20 11:09:31 -0400

    Fix to email

--- Permissions/Email.cs
+++ Permissions/Email.cs
@@ -15,13 +15,10 @@ namespace SitePermissions
         public static async Task<List<Tuple<Microsoft.Graph.User, bool>>> InformOwners(ICollection<Site> sites, GraphServiceClient graphAPIAuth,  ILogger log)
         {
             var results = new List<Tuple<Microsoft.Graph.User, bool>>();
-            var siteOwners = new List<Microsoft.Graph.User>();
-            string sitename = "";
-            string siteurl = "";
+            var siteOwnersInfo = new List<Tuple<Microsoft.Graph.User, string, string>>();
+
             foreach (var site in sites)
             {
-                sitename = site.Name;
-                siteurl = site.WebUrl;
                 var groupQueryOptions = new List<QueryOption>()
                 {
                     new QueryOption("$search", "\"mailNickname:" + site.Name +"\"")
@@ -51,7 +48,7 @@ namespace SitePermissions
 
                                 if (user != null)
                                 {
-                                    siteOwners.Add(user);
+                                    siteOwnersInfo.Add(new Tuple<Microsoft.Graph.User, string, string>(user, site.Name, site.WebUrl));
                                 }
                             }
                         }
@@ -61,10 +58,10 @@ namespace SitePermissions
                 while (groups.NextPageRequest != null && (groups = await groups.NextPageRequest.GetAsync()).Count > 0);
             }
 
-            foreach (var owner in siteOwners.Distinct())
+            foreach (var ownerInfo in siteOwnersInfo)
             {
-                var result = await SendMisconfiguredEmail(owner.DisplayName, owner.Mail, sitename, siteurl, log);
-                results.Add(new Tuple<Microsoft.Graph.User, bool>(owner, result));
+                var result = await SendMisconfiguredEmail(ownerInfo.Item1.DisplayName, ownerInfo.Item1.Mail, ownerInfo.Item2, ownerInfo.Item3, log);
+                results.Add(new Tuple<Microsoft.Graph.User, bool>(ownerInfo.Item1, result));
             }
             return results;
         }
@@ -87,37 +84,37 @@ namespace SitePermissions
                         ContentType = BodyType.Html,
                         Content = @$"(La version française suit)<br><br>
 
-Dear { Username },<br><br> 
-
-Thank you for your interest and participation as a Community Owner on the GCXchange platform.<br><br> 
-
-Communities continue to be our most popular feature of the platform, and we look forward to seeing new and interesting communities appear as the platform grows.<br><br>
-
-One of the key features of GCXchange is the open by default model which allows for seamless cross-departmental collaboration. We encourage all site owners to maintain the open by default practice as outlined in our Terms of Use.<br><br>
-
-The GCXchange Team received a notification that the permissions were modified to the Community space in which you are Owner: <a href='{siteurl}'>{sitename}</a>. This action goes against the open by default model of the platform. Please note that the default settings will now be reapplied, and we would like to give you a friendly reminder to please no longer change these permissions.<br><br> 
-
-Please let us know if you have any questions or concerns, and once again thank you for being a valued member of GCXchange.<br><br>
- 
-Regards,<br>
-The GCX Team <br><br>
-
---------------------------------------<br><br>
-
-Bonjour { Username },<br><br>
-
-Nous vous remercions de l’intérêt que vous manifestez à l’égard de la plateforme GCÉchange, de même que de votre participation en tant que responsable de l’une de ses collectivités. <br><br>
-
-Les collectivités demeurent la fonction la plus prisée de la plateforme, et nous sommes impatients de voir de nouvelles collectivités intéressantes se créer, au fur et à mesure que la plateforme évolue. <br><br>
-
-L’une des principales caractéristiques de GCÉchange est son modèle ouvert par défaut, qui permet une collaboration permanente entre les ministères. Nous encourageons tous les responsables à maintenir la pratique d’ouverture par défaut, comme indiqué dans nos conditions d’utilisation. <br><br>
-
-L’équipe de GCÉchange a reçu un avis concernant la modification des autorisations relatives à la collectivité dont vous êtes responsable: <a href='{siteurl}'>{sitename}</a>. Cette modification va à l’encontre du modèle ouvert par défaut de la plateforme. Veuillez prendre note que les paramètres par défaut seront rétablis, et nous vous prions de ne plus modifier les autorisations.<br><br>
-
-N’hésitez pas à nous faire part de vos questions et de vos préoccupations. Vous êtes un membre important de GCÉchange et nous souhaitons réitérer notre gratitude.<br><br>  
- 
-Nous vous prions d’agréer l’expression de nos sentiments les meilleurs. <br>
-Équipe de GCÉchange"
+                        Dear { Username },<br><br> 
+                        
+                        Thank you for your interest and participation as a Community Owner on the GCXchange platform.<br><br> 
+                        
+                        Communities continue to be our most popular feature of the platform, and we look forward to seeing new and interesting communities appear as the platform grows.<br><br>
+                        
+                        One of the key features of GCXchange is the open by default model which allows for seamless cross-departmental collaboration. We encourage all site owners to maintain the open by default practice as outlined in our Terms of Use.<br><br>
+                        
+                        The GCXchange Team received a notification that the permissions were modified to the Community space in which you are Owner: <a href='{siteurl}'>{sitename}</a>. This action goes against the open by default model of the platform. Please note that the default settings will now be reapplied, and we would like to give you a friendly reminder to please no longer change these permissions.<br><br> 
+                        
+                        Please let us know if you have any questions or concerns, and once again thank you for being a valued member of GCXchange.<br><br>
+                         
+                        Regards,<br>
+                        The GCX Team <br><br>
+                        
+                        --------------------------------------<br><br>
+                        
+                        Bonjour { Username },<br><br>
+                        
+                        Nous vous remercions de l’intérêt que vous manifestez à l’égard de la plateforme GCÉchange, de même que de votre participation en tant que responsable de l’une de ses collectivités. <br><br>
+                        
+                        Les collectivités demeurent la fonction la plus prisée de la plateforme, et nous sommes impatients de voir de nouvelles collectivités intéressantes se créer, au fur et à mesure que la plateforme évolue. <br><br>
+                        
+                        L’une des principales caractéristiques de GCÉchange est son modèle ouvert par défaut, qui permet une collaboration permanente entre les ministères. Nous encourageons tous les responsables à maintenir la pratique d’ouverture par défaut, comme indiqué dans nos conditions d’utilisation. <br><br>
+                        
+                        L’équipe de GCÉchange a reçu un avis concernant la modification des autorisations relatives à la collectivité dont vous êtes responsable: <a href='{siteurl}'>{sitename}</a>. Cette modification va à l’encontre du modèle ouvert par défaut de la plateforme. Veuillez prendre note que les paramètres par défaut seront rétablis, et nous vous prions de ne plus modifier les autorisations.<br><br>
+                        
+                        N’hésitez pas à nous faire part de vos questions et de vos préoccupations. Vous êtes un membre important de GCÉchange et nous souhaitons réitérer notre gratitude.<br><br>  
+                         
+                        Nous vous prions d’agréer l’expression de nos sentiments les meilleurs. <br>
+                        Équipe de GCÉchange"
                     },
                     ToRecipients = new List<Recipient>()
                     {
